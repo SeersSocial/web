786bceff8bb4506a64bd27d686dc1f23f8bcc5db
Marcio Diaz <marcio.diaz@gmail.com>
2023-09-11 12:34:36 +0100

    More white text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,22 +30,22 @@
 			<div>
 			  <h2 class="mb-6 text-sm font-semibold uppercase">Resources</h2>
 			  <FooterLinkGroup>
-				<FooterLink liClass="mb-4" href="/">Documentation</FooterLink>
-				<FooterLink liClass="mb-4" href="/">FAQ</FooterLink>
+				<FooterLink liClass="mb-4" href="/" style="color:white">Documentation</FooterLink>
+				<FooterLink liClass="mb-4" href="/" style="color:white">FAQ</FooterLink>
 			  </FooterLinkGroup>
 			</div>
 			<div>
 			  <h2 class="mb-6 text-sm font-semibold uppercase dark:text-white">Follow us</h2>
 			  <FooterLinkGroup>
-				<FooterLink liClass="mb-4" target="_blank" href="https://twitter.com/icx_one">Twitter</FooterLink>
+				<FooterLink liClass="mb-4" target="_blank"  style="color:white" href="https://twitter.com/icx_one">Twitter</FooterLink>
 				<!-- <FooterLink liClass="mb-4" href="/">Discord</FooterLink> -->
 			  </FooterLinkGroup>
 			</div>
 			<div>
 			  <h2 class="mb-6 text-sm font-semibold uppercase dark:text-white">Legal</h2>
 			  <FooterLinkGroup>
-				<FooterLink liClass="mb-4" target="_blank" href="https://docs.google.com/document/d/1NkaGXvzFCLV5ep9jQQU3WaY5Gcxqh0ihekymImckhAo/edit?usp=sharing">Privacy Policy</FooterLink>
-				<FooterLink liClass="mb-4" target="_blank" href="https://docs.google.com/document/d/15iAz0gxVEj1ZKvlxiRaT2QlR0ITm8AGVGsvAj15uyFk/edit?usp=sharing">Terms & Conditions</FooterLink>
+				<FooterLink liClass="mb-4" target="_blank"  style="color:white" href="https://docs.google.com/document/d/1NkaGXvzFCLV5ep9jQQU3WaY5Gcxqh0ihekymImckhAo/edit?usp=sharing">Privacy Policy</FooterLink>
+				<FooterLink liClass="mb-4" target="_blank"  style="color:white" href="https://docs.google.com/document/d/15iAz0gxVEj1ZKvlxiRaT2QlR0ITm8AGVGsvAj15uyFk/edit?usp=sharing">Terms & Conditions</FooterLink>
 			  </FooterLinkGroup>
 			</div>
 		  </div>
@@ -54,7 +54,7 @@
 		<div class="sm:flex sm:items-center sm:justify-between">
 		  <FooterCopyright href="/" by="ICX." />
 		  <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-			<FooterIcon href="/" class="text-white-500">
+			<FooterIcon href="/" class="text-white">
 			<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
 			</FooterIcon>
 			<FooterIcon href="/" class="text-white">
