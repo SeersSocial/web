cc6f13cb24d3bfaa45afc37c6ef3b14011caa112
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 08:52:34 +0100

    Change key features color

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,12 +15,12 @@
 		<img src={network} alt="" class="rounded h-200" />
 		<h2 class="font-bold text-sky-500 mt-2">Key Features</h2>
 		<ul class="list-inside space-y-4">
-			<li><span class="text-sky-300">True Ownership:</span> On ICX, your content is yours. Leveraging the principles of web3, users have true ownership of their posts, images, and videos. This means you can benefit directly from the value your content generates, without intermediaries taking a disproportionate cut.</li>
-			<li><span class="text-sky-300">Community Governance:</span> ICX is not just a platform; it's a community. Decisions about the network's rules, algorithms, and future developments are made collectively by its users. Your voice genuinely matters here.</li>
-			<li><span class="text-sky-300">Enhanced Privacy:</span> Say goodbye to invasive ads and data mining. ICX values your privacy. With end-to-end encryption and VetKeys protocols, your personal information and conversations remain confidential.</li>
-			<li><span class="text-sky-300">Token-Based Economy:</span> ICX integrates a native token system, allowing users to earn, spend, or trade tokens based on their activity and contributions to the platform. Whether you're a content creator or an active participant in discussions, there's an opportunity to earn.</li>
-			<li><span class="text-sky-300">Interoperability:</span> Being built on the Internet Computer, ICX offers seamless integration with other decentralized apps and platforms. This ensures that as the web3 ecosystem grows, ICX users will be at the forefront of innovation and connectivity.</li>
-			<li><span class="text-sky-300">Resilient & Censorship-Resistant:</span> Thanks to its decentralized infrastructure, ICX is highly resistant to attacks, censorship, and centralized control. No matter where you are in the world, your voice can be heard without fear of suppression.</li>
+			<li><span class="text-emerald-400">True Ownership:</span> On ICX, your content is yours. Leveraging the principles of web3, users have true ownership of their posts, images, and videos. This means you can benefit directly from the value your content generates, without intermediaries taking a disproportionate cut.</li>
+			<li><span class="text-emerald-400">Community Governance:</span> ICX is not just a platform; it's a community. Decisions about the network's rules, algorithms, and future developments are made collectively by its users. Your voice genuinely matters here.</li>
+			<li><span class="text-emerald-400">Enhanced Privacy:</span> Say goodbye to invasive ads and data mining. ICX values your privacy. With end-to-end encryption and VetKeys protocols, your personal information and conversations remain confidential.</li>
+			<li><span class="text-emerald-400">Token-Based Economy:</span> ICX integrates a native token system, allowing users to earn, spend, or trade tokens based on their activity and contributions to the platform. Whether you're a content creator or an active participant in discussions, there's an opportunity to earn.</li>
+			<li><span class="text-emerald-400">Interoperability:</span> Being built on the Internet Computer, ICX offers seamless integration with other decentralized apps and platforms. This ensures that as the web3 ecosystem grows, ICX users will be at the forefront of innovation and connectivity.</li>
+			<li><span class="text-emerald-400">Resilient & Censorship-Resistant:</span> Thanks to its decentralized infrastructure, ICX is highly resistant to attacks, censorship, and centralized control. No matter where you are in the world, your voice can be heard without fear of suppression.</li>
 		</ul>
 		<h2 class="font-bold text-sky-500 mt-2">Join the Revolution</h2>
 		<p class="mb-4">
