56f1122f2b36c2e9d00466dbe6cd5e8c8626dcef
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 08:58:33 +0100

    Change revolution color

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,7 +13,7 @@
 		<h2 class="font-bold text-sky-500">What is ICX?</h2>
 		<p class="">ICX is a cutting-edge web3 social network designed to embody the true principles of a decentralized internet. Built atop the revolutionary framework of the Internet Computer, it mirrors the functionalities you love on platforms like Twitter, but with enhanced privacy, ownership, and community-driven governance.</p>
 		<img src={network} alt="" class="rounded h-200" />
-		<h2 class="font-bold text-sky-500 mt-2">Key Features</h2>
+		<h2 class="font-bold text-emerald-500 mt-2">Key Features</h2>
 		<ul class="list-inside space-y-4">
 			<li><span class="text-emerald-400">True Ownership:</span> On ICX, your content is yours. Leveraging the principles of web3, users have true ownership of their posts, images, and videos. This means you can benefit directly from the value your content generates, without intermediaries taking a disproportionate cut.</li>
 			<li><span class="text-emerald-400">Community Governance:</span> ICX is not just a platform; it's a community. Decisions about the network's rules, algorithms, and future developments are made collectively by its users. Your voice genuinely matters here.</li>
@@ -22,7 +22,7 @@
 			<li><span class="text-emerald-400">Interoperability:</span> Being built on the Internet Computer, ICX offers seamless integration with other decentralized apps and platforms. This ensures that as the web3 ecosystem grows, ICX users will be at the forefront of innovation and connectivity.</li>
 			<li><span class="text-emerald-400">Resilient & Censorship-Resistant:</span> Thanks to its decentralized infrastructure, ICX is highly resistant to attacks, censorship, and centralized control. No matter where you are in the world, your voice can be heard without fear of suppression.</li>
 		</ul>
-		<h2 class="font-bold text-sky-500 mt-2">Join the Revolution</h2>
+		<h2 class="font-bold text-pink-500 mt-2">Join the Revolution</h2>
 		<p class="mb-4">
 			The internet is evolving, and ICX is at the forefront of this change. Join us in shaping the future of social networking, where power returns to the people, and creativity thrives unbounded.
 		</p>
