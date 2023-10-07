a5b3604a06f985db1f2d877637b117d97cb09ba7
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 21:55:13 +0100

    Start fixing menu

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -14,14 +14,13 @@
 		Skeleton,
 		TextPlaceholder
 	} from 'flowbite-svelte';
+	$: activeUrl = $page.url.pathname;
 </script>
 
 <div class="relative px-8 bg-black text-white">
 	<Navbar
 		style="background:transparent; color: white;"
 		navClass="bg-black text-white px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0"
-		let:hidden
-		let:toggle
 	>
 		<NavBrand href="/">
 			<img
@@ -32,7 +31,7 @@
 			<span class="self-center whitespace-nowrap text-xl font-semibold">ICX</span>
 		</NavBrand>
 		<NavHamburger  style="color:white" />
-		<NavUl {hidden} style="color:white">
+		<NavUl {activeUrl} style="color:white">
 			<NavLi href="/" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Home</NavLi>
 			<NavLi href="/about" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">About</NavLi>
 			<!-- <NavLi href="/sns">SNS Whitepaper</NavLi> -->
