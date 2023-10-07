867816838544eaee8a80fbb14ead24fd84e7dc0b
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 08:40:00 +0100

    Move image up

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,6 +11,7 @@
 	<div class="flex flex-col md:w-2/3 gap-4">
 		<h2 class="font-bold text-sky-500">What is ICX?</h2>
 		<p class="">ICX is a cutting-edge web3 social network designed to embody the true principles of a decentralized internet. Built atop the revolutionary framework of the Internet Computer, it mirrors the functionalities you love on platforms like Twitter, but with enhanced privacy, ownership, and community-driven governance.</p>
+		<img src={network} alt="" class="rounded h-200" />
 		<h2 class="font-bold text-sky-500 mt-2">Key Features</h2>
 		<ul class="list-inside space-y-4">
 			<li><span class="text-sky-300">True Ownership:</span> On ICX, your content is yours. Leveraging the principles of web3, users have true ownership of their posts, images, and videos. This means you can benefit directly from the value your content generates, without intermediaries taking a disproportionate cut.</li>
@@ -24,6 +25,5 @@
 		<p class="mb-4">
 			The internet is evolving, and ICX is at the forefront of this change. Join us in shaping the future of social networking, where power returns to the people, and creativity thrives unbounded.
 		</p>
-		<img src={network} alt="" class="rounded" />
 	</div>
 </section>
