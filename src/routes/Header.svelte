722aa7b4be1a698ea2bde88c349226eba7594a0b
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 23:54:22 +0100

    Fix

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -33,7 +33,7 @@
 			<span class="self-center whitespace-nowrap text-xl font-semibold">ICX</span>
 		</NavBrand>
 		<NavHamburger style="background:black" on:click={toggle} class="" btnClass="text-white focus:outline-none whitespace-normal m-0.5 rounded-lg focus:ring-2 p-1.5 focus:ring-gray-400  bg-black hover:bg-black  ml-3 md:hidden" />
-		<NavUl {hidden} style="background:black" ulClass="bg-black dark:bg-black text-white dark:text-white rounded-lg border border-black dark:border-black flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+		<NavUl {hidden} style="background:black" activeClass="bg-black" nonActiveClass="bg-black" ulClass="bg-black dark:bg-black text-white dark:text-white rounded-lg border border-black dark:border-black flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 			<NavLi href="/" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Home</NavLi>
 			<NavLi href="/about" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">About</NavLi>
 			<NavLi href="/tokenomics" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Token</NavLi>
