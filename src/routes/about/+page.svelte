daadbfe9b16c1cfaac3f26769f8d4cf20dab8f0e
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 08:11:05 +0100

    Change about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,43 +1,25 @@
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About ICX" />
 </svelte:head>
 
 <section class="w-full flex flex-col mt-14 text-lg justify-center justify-items-center content-center items-center">
 
 	<div class="flex flex-col md:w-2/3 gap-4">
-		<div>
-		<h3 class="font-bold text-sky-500">About</h3>
-		<p class="indent-8">ICX is a web3 social network developed by a cohesive group of engineers who joined forces in 2022. 
-		They have built a strong professional bond through years of collaboration in the Web3 industry and their shared academic experiences. 
-		The team comprises individuals with distinguished qualifications, including holders of PhD degrees, former Googlers, and winners of programming Olympiads.
+		<h3 class="font-bold text-sky-500">What is ICX?</h3>
+		<p class="indent-8">ICX is a cutting-edge web3 social network designed to embody the true principles of a decentralized internet. Built atop the revolutionary framework of the Internet Computer, it mirrors the functionalities you love on platforms like Twitter, but with enhanced privacy, ownership, and community-driven governance.</p>
+		<h3 class="font-bold text-sky-500">Key Features</h3>
+		<ul class="list-decimal list-inside space-y-4">
+			<li><span class="font-semibold text-sky-500">True Ownership:</span> On ICX, your content is yours. Leveraging the principles of web3, users have true ownership of their posts, images, and videos. This means you can benefit directly from the value your content generates, without intermediaries taking a disproportionate cut.</li>
+			<li><span class="font-semibold text-sky-500">Community Governance:</span> ICX is not just a platform; it's a community. Decisions about the network's rules, algorithms, and future developments are made collectively by its users. Your voice genuinely matters here.</li>
+			<li><span class="font-semibold text-sky-500">Enhanced Privacy:</span>Say goodbye to invasive ads and data mining. ICX values your privacy. With end-to-end encryption and VetKeys protocols, your personal information and conversations remain confidential.</li>
+			<li><span class="font-semibold text-sky-500">Token-Based Economy:</span> ICX integrates a native token system, allowing users to earn, spend, or trade tokens based on their activity and contributions to the platform. Whether you're a content creator or an active participant in discussions, there's an opportunity to earn.</li>
+			<li><span class="font-semibold text-sky-500">Interoperability:</span> Being built on the Internet Computer, ICX offers seamless integration with other decentralized apps and platforms. This ensures that as the web3 ecosystem grows, ICX users will be at the forefront of innovation and connectivity.</li>
+			<li><span class="font-semibold text-sky-500">Resilient & Censorship-Resistant:</span>Thanks to its decentralized infrastructure, ICX is highly resistant to attacks, censorship, and centralized control. No matter where you are in the world, your voice can be heard without fear of suppression.</li>
+		</ul>
+		<h3 class="font-bold text-sky-500">Join the Revolution</h3>
+		<p class="pt-4 indent-8">
+			The internet is evolving, and ICX is at the forefront of this change. Join us in shaping the future of social networking, where power returns to the people, and creativity thrives unbounded.
 		</p>
-		</div>
-		<div>
-		<h3 class="font-bold text-sky-500">Founder</h3>
-		<p class="indent-8">
-			The founder of ICX has over 3 years of experience developing web3 software and more than 10 years of experience in web software development. 
-			He began his web3 journey as a Rust Core Dev, contributing to the development of Polkadot alongside Gavin Wood (Ethereum co-founder). 
-			Additionally, he has an extensive academic background, having worked as a PhD student specializing in the development of algorithms and proving complexity theorems in the area of Automata for Concurrent Program Verification.
-		</p>
-		</div>
-
-		<div>
-		<h3 class="font-bold text-sky-500">Manifesto: Unleashing the Cyberpunk Web3 Revolution</h3>
-		<p class="indent-8">
-We, the pioneers of the cyberpunk era, gather here to establish a new paradigm for social networking—a digital realm where truth, ownership, privacy, and sovereignty reign supreme. Guided by the spirit of decentralization and empowered by Web3 technologies, we envision a future where individuals regain control over their online lives, liberated from the chains of centralized surveillance and manipulation.
-</p>
-<ul class="list-decimal list-inside">
-	<li><span class="font-semibold">Truth as the Foundation:</span> In our cyberpunk web3 social network, truth is the bedrock upon which we build. We reject the era of misinformation, propaganda, and algorithmic manipulation. We vow to foster an environment where authenticity, transparency, and fact-checking prevail. Let us honor truth, enabling genuine connections and empowering individuals to make informed decisions.</li>
-	<li><span class="font-semibold">Empowering Ownership:</span> The power dynamic shifts within our web3 social network. We champion the concept of true ownership, where users control their data, identities, and digital assets. No longer shall centralized entities profit from exploiting our personal information. Through decentralized technologies, we reclaim our digital possessions and harness them for our benefit, fueling innovation and prosperity for all.</li>
-	<li><span class="font-semibold">Privacy as a Fundamental Right:</span> In this cyberpunk future, privacy is not a luxury but an inalienable right. We establish an unwavering commitment to protecting the personal data and online activities of our users. No surveillance, no tracking, and no invasive profiling. We foster a safe haven where individuals can express themselves freely without fear of retribution or intrusion.</li>
-	<li><span class="font-semibold">Sovereignty over Digital Existence:</span> Our web3 social network liberates individuals from the clutches of corporate dominance. We forge a path toward digital sovereignty, where users have full control over their online identities and experiences. No longer shall we be mere data points on a corporate spreadsheet. We champion a user-centric approach, empowering individuals to shape their digital presence and participate in the governance of the platform.</li>
-	<li><span class="font-semibold">Embracing Cyberpunk Aesthetics:</span> Our web3 social network embodies the essence of the cyberpunk ethos. We embrace the dark, neon-lit landscapes of a dystopian future, where technology and rebellion coexist. Through a visually immersive experience, we seek to inspire and challenge the status quo, sparking a collective awakening that transcends boundaries.</li>
-</ul>
-<p class="pt-4 class="indent-8"">
-Together, we embark on a mission to redefine social networking, ushering in a new era of cyberpunk-inspired principles. By upholding truth, ownership, privacy, and sovereignty, we will forge a future where the power is in the hands of the individual. Join us in this revolution, where the virtual world is liberated, and a brighter, more authentic future awaits.	
-		</p>
-	</div>
-		
 	</div>
 </section>
