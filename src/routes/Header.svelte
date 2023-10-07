804d3ae78385b7dd29b7c9cda2e34457231f9c26
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-07 22:08:20 +0100

    Re add hidden and toogle

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -15,6 +15,8 @@
 	<Navbar
 		style="background:transparent; color: white;"
 		navClass="bg-black text-white px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0"
+		let:hidden
+		let:toggle
 	>
 		<NavBrand href="/">
 			<img
@@ -25,7 +27,7 @@
 			<span class="self-center whitespace-nowrap text-xl font-semibold">ICX</span>
 		</NavBrand>
 		<NavHamburger  style="color:white" btnClass="bg-black hover:bg-black"/>
-		<NavUl style="color:white">
+		<NavUl {hidden} style="color:white">
 			<NavLi href="/" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Home</NavLi>
 			<NavLi href="/about" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">About</NavLi>
 		</NavUl>
