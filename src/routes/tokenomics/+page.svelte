86adbbbc3b359c124b17118bf188cf3b715354c2
Marcio Diaz <marcio.diaz@gmail.com>
2023-10-08 11:22:42 +0100

    Improve token

--- src/routes/tokenomics/+page.svelte
+++ src/routes/tokenomics/+page.svelte
@@ -22,20 +22,21 @@
 		<p>
 			The ICX token model takes shape around a meticulously devised inflationary curve, inaugurating with an inflation rate of 10%. This rate is designed to gracefully taper, converging towards 0% over a clear-cut 12-year duration.
 		</p>
-        <img src={chart} alt="" class="rounded" />
-		<h2 class="font-bold text-pink-500 mt-2">Comparison with Bitcoin's Monetary Policy</h2>
+        <h3 class="font-bold mt-2">Comparison with Bitcoin's Monetary Policy</h3>
 		<p class="mb-4 font-medium">
 			While Bitcoin has established its legacy in the cryptocurrency realm with its deflationary issuance model marked by periodic halvings, ICX offers a unique paradigm. It champions a uniform and measured reduction in inflation, crafting a predictable course over its 12-year timeline.
 		</p>
-        <h2 class="font-bold text-sky-500">Advantages</h2>
+        <img src={chart} alt="" class="rounded" />
+		<h2 class="font-bold text-sky-500">Advantages</h2>
         <ul class="list-inside space-y-4">
 			<li><span class="underline decoration-sky-400">Incentivized Engagement:</span> The tokenomics fosters network participation, with the initial inflation rate tailored to boost active engagement and community growth.</li>
 			<li><span class="underline decoration-sky-400">Balanced Economic Model:</span> The ICX model strives for consistent growth, avoiding drastic changes that might lead to market volatility.</li>
 			<li><span class="underline decoration-sky-400">Stakeholder Alignment:</span> By having a clear token allocation strategy, ICX ensures vested interest among developers, community members, and strategic planners in the project's success.</li>
             <li><span class="underline decoration-sky-400">Future-Proofed Treasury:</span> The treasury's significant token allocation ensures a robust reserve for pivots, expansions, or unforeseen challenges, securing the network's future.</li>
         </ul>
+        <h2 class="font-bold text-pink-500">Conclusion</h2>
         <p class="mb-4 font-medium">
-            In conclusion, ICX's tokenomics is a meticulous fusion of strategic planning and community-driven priorities. It's designed to offer both immediate incentives for participation and the foundation for long-term network sustainability. Importantly, the ICX token serves as a utility within its ecosystem, and its distribution or use should not be viewed as a speculative instrument or a security.
+            ICX's tokenomics is a meticulous fusion of strategic planning and community-driven priorities. It's designed to offer both immediate incentives for participation and the foundation for long-term network sustainability. Importantly, the ICX token serves as a utility within its ecosystem, and its distribution or use should not be viewed as a speculative instrument or a security.
         </p>
     </div>
 </section>
