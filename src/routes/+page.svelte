05c3592e67b6cb1657bebed951f20ead6fcab58e
Mar Pense <marcio@pense.dev>
2024-03-06 13:18:30 +0000

    Fix countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,19 +59,19 @@
   <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
     <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
       <span class="countdown font-mono text-5xl">
-        <span style="--value:15;"></span>
+        <span >30</span>
       </span>
       days
     </div> 
     <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
       <span class="countdown font-mono text-5xl">
-        <span style="--value:10;"></span>
+        <span >12</span>
       </span>
       hours
     </div> 
     <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
       <span class="countdown font-mono text-5xl">
-        <span style="--value:24;"></span>
+        <span >48</span>
       </span>
       min
     </div> 
