4480af2738d7814cd83a6bbbd192cf943adf3767
Mar Pense <marcio@pense.dev>
2024-03-06 13:10:48 +0000

    Improve countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,28 +56,28 @@
     Countdown to release
   </div>
 
-  <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
-    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-      <span class="countdown font-mono text-5xl">
-        <span style="--value:15;"></span>
+  <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span className="countdown font-mono text-5xl">
+        <span style={{"--value":15}}></span>
       </span>
       days
     </div> 
-    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-      <span class="countdown font-mono text-5xl">
-        <span style="--value:10;"></span>
+    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span className="countdown font-mono text-5xl">
+        <span style={{"--value":10}}></span>
       </span>
       hours
     </div> 
-    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-      <span class="countdown font-mono text-5xl">
-        <span style="--value:24;"></span>
+    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span className="countdown font-mono text-5xl">
+        <span style={{"--value":24}}></span>
       </span>
       min
     </div> 
-    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-      <span class="countdown font-mono text-5xl">
-        <span style="--value:2;"></span>
+    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span className="countdown font-mono text-5xl">
+        <span style={{"--value":33}}></span>
       </span>
       sec
     </div>
