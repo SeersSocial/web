d7d9e05649056a37e274b2e62c7eea4dacd090d6
Mar Pense <marcio@pense.dev>
2024-03-06 14:00:39 +0000

    Change date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@ let minutes = 0;
 let seconds = 0;
 
 function updateCountdown() {
- const targetDate = new Date('2024-05-01T00:00:00').getTime(); // Set your target date/time here
+ const targetDate = new Date('2024-05-10T00:00:00').getTime(); // Set your target date/time here
   const now = new Date().getTime();
   const difference = targetDate - now;
 
