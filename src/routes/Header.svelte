b6175c5ab6b9b24ffecd738406b0a8ad8cd4f272
Mar Pense <marcio@pense.dev>
2024-03-06 11:28:46 +0000

    Min logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -33,7 +33,7 @@
 		<NavBrand href="/" class="border-none focus:outline-none hover:border-none focus:border-none">
 			<img
 				src={logo3}
-				class="mr-3 bg-transparent w-12"
+				class="mr-3 bg-transparent w-10"
 				alt=""
 			/>
 			<!-- <span class="self-center whitespace-nowrap text-xl font-semibold">ICX</span> -->
