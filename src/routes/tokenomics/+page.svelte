8ad57c4430ad21c0822bb646ec79e5f04af8639b
Mar Pense <marcio@pense.dev>
2024-03-07 12:52:33 +0000

    Add dashboard link

--- src/routes/tokenomics/+page.svelte
+++ src/routes/tokenomics/+page.svelte
@@ -23,4 +23,7 @@
 		<h2 class="font-bold text-[#29ABE2]">Decentralisation</h2>
 		<p class="">Every year, a portion of the treasury will be used to reward users of the dApp by distributing neurons. In this way, the DAO will become progressively more decentralized.</p>
 	</div>
+	<div class="flex flex-col md:w-2/3 gap-4 mt-2">
+		You can track governance parameteres and follow token transactions and neuron activity in the <a href="https://dashboard.internetcomputer.org/sns/u67kc-jyaaa-aaaaq-aabpq-cai">dashboard of our SNS</a>.
+	</div>
 </section>
