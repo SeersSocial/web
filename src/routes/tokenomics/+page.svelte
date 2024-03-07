e8c481b2b77ee06ea5b8fdf3e233511612a54adf
Mar Pense <marcio@pense.dev>
2024-03-07 14:11:45 +0000

    Improve paragraph

--- src/routes/tokenomics/+page.svelte
+++ src/routes/tokenomics/+page.svelte
@@ -12,7 +12,7 @@
 
 	<div class="flex flex-col md:w-2/3 gap-4">
 		<h2 class="font-bold text-[#29ABE2]">Token Economics</h2>
-		<p class="">The heart of ICX tokenomics revolves around avoiding inflation by keeping a fixed supply of 100 million ICX tokens. The distribution of these tokens is delineated as follows:</p>
+		<p class="">The core of ICX token economy is centered on preventing inflation by maintaining a capped supply of 100 million ICX tokens. To incentivize deflation and enhance the value of the network, the protocol requires the burning of tokens to utilize the dapp, thereby reducing the total supply over time. The distribution of ICX tokens is currently delineated as follows:</p>
 		<ul class="list-inside space-y-4">
 			<li><span class="text-[#FBB03B]">Dev Team:</span> 15% of the total supply, equating to 15 million ICX tokens, is reserved for the development team. These tokens are vested over a span of 4 years, underscoring the team's long-term dedication to the project and synchronizing their interests with the continued evolution and success of the platform.</li>
 			<li><span class="text-[#FBB03B]">Community:</span> 17% of the tokens, amounting to 17 million ICX tokens, are held by the broader community. This allocation is devised to amplify active participation and nurture a deep-rooted sense of ownership and collaborative decision-making within the ecosystem.</li>
