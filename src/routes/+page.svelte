cf9e75c0e8f8cd4e0cd9230b0c66d5547df4ff2e
Mar Pense <marcio@pense.dev>
2024-03-07 11:52:49 +0000

    More details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,7 +78,7 @@ function updateCountdown() {
 
 <section class="w-full flex flex-col gap-4 md:gap-8 text-2xl justify-center mt-2 p-2">
 	<div class="text-2xl font-bold text-center">
-    Countdown to release
+    Countdown to testnet release
   </div>
 
   <div class="grid grid-flow-col gap-5 text-center auto-cols-max justify-center">
