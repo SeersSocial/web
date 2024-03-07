87865c839f2c03d85d37f9207886297252a85441
Mar Pense <marcio@pense.dev>
2024-03-07 19:22:44 +0000

    u

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,7 +25,7 @@
 <div class="relative px-2 bg-black text-white">
 	<Navbar
 		style="background:transparent; color: white;"
-		navClass="bg-[#1E005D] text-white px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0"
+		navClass="bg-black text-white px-2 sm:px-4 py-2.5 absolute w-full z-20 top-0 left-0"
 		let:hidden
 		let:toggle
 		color="dark"
@@ -38,11 +38,11 @@
 			/>
 			<!-- <span class="self-center whitespace-nowrap text-xl font-semibold">ICX</span> -->
 		</NavBrand>
-		<NavHamburger style="background:#1E005D" on:click={toggle} class="bg-[#1E005D] border-none outline-none" btnClass="border-none outline-none text-white focus:outline-none whitespace-normal m-0.5 rounded-lg p-1.5 bg-[#1E005D] hover:bg-[#1E005D]  ml-3 md:hidden" />
-		<NavUl {hidden} style="background:#1E005D"  class="bg-[#1E005D]" ulClass="p-0 bg-[#1E005D] text-white dark:text-white rounded-lg border border-[#1E005D] flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-			<NavLi on:click={() => { if (width < 1024) toggle() }} href="/" activeClass="bg-[#1E005D] text-white" nonActiveClass="bg-[#1E005D] text-white">Home</NavLi>
+		<NavHamburger style="background:black" on:click={toggle} class="bg-black border-none outline-none" btnClass="border-none outline-none text-white focus:outline-none whitespace-normal m-0.5 rounded-lg p-1.5 bg-black hover:bg-black  ml-3 md:hidden" />
+		<NavUl {hidden} style="background:black"  class="bg-black" ulClass="p-0 bg-black text-white dark:text-white rounded-lg border border-[black] flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+			<NavLi on:click={() => { if (width < 1024) toggle() }} href="/" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Home</NavLi>
 			<!-- <NavLi on:click={() => { if (width < 1024) toggle() }} href="/about" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">About</NavLi> -->
-			<NavLi on:click={() => { if (width < 1024) toggle() }} href="/tokenomics" activeClass="bg-[#1E005D] text-white" nonActiveClass="bg-[#1E005D] text-white">Token</NavLi>
+			<NavLi on:click={() => { if (width < 1024) toggle() }} href="/tokenomics" activeClass="bg-black text-white" nonActiveClass="bg-black text-white">Token</NavLi>
 		</NavUl>
 	</Navbar>
 </div>
