d5d491f3caa722ae7414f32cf5a66cd902330b21
Mar Pense <marcio@pense.dev>
2024-03-07 13:40:43 +0000

    Add green tick

--- src/routes/Roadmap.svelte
+++ src/routes/Roadmap.svelte
@@ -13,12 +13,12 @@
                 <div class="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full" aria-hidden="true"></div>
 
                 <div class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 rounded-full shadow">
-                    <svg class="w-10 h-10 text-[#522785]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="w-10 h-10 text-[#29ABE2]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
                 </div>
                 <div class="ml-6">
-                    <h3 class="text-lg font-semibold text-[#522785]">Decentralise ICX using the SNS</h3>
+                    <h3 class="text-lg font-semibold text-[#29ABE2]">Decentralise ICX using the SNS ✅</h3>
                     <p class="mt-4 text-base text-white-100">Decentralize ICX by distributing 31% of the token supply and establishing an SNS DAO.</p>
                 </div>
             </li>
@@ -27,24 +27,24 @@
                 <div class="-ml-0.5 absolute mt-0.5 top-14 left-8 w-px border-l-4 border-dotted border-gray-300 h-full" aria-hidden="true"></div>
 
                 <div class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 rounded-full shadow">
-                    <svg class="w-10 h-10 text-[#522785]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="w-10 h-10 text-[#ED1E79]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
                     </svg>
                 </div>
                 <div class="ml-6">
-                    <h3 class="text-lg font-semibold text-[#522785]">Improve UI/UX and X features</h3>
+                    <h3 class="text-lg font-semibold text-[#ED1E79]">Improve UI/UX and X features</h3>
                     <p class="mt-4 text-base text-white-600">Complete the new UI/UX implementation and refine all core X features.</p>
                 </div>
             </li>
 
             <li class="relative flex items-start">
                 <div class="relative flex items-center justify-center flex-shrink-0 w-16 h-16 rounded-full shadow">
-                    <svg class="w-10 h-10 text-[#522785]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="w-10 h-10 text-[#F15A24]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M13 10V3L4 14h7v7l9-11h-7z" />
                     </svg>
                 </div>
                 <div class="ml-6">
-                    <h3 class="text-lg font-semibold text-[#522785]">Re-launch prediction markets</h3>
+                    <h3 class="text-lg font-semibold text-[#F15A24]">Re-launch prediction markets</h3>
                     <p class="mt-4 text-base text-white-600">Implement the resolution of prediction markets through token voting and a decentralized sports oracle, then re-enable prediction markets.</p>
                 </div>
             </li>
