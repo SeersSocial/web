e336cd81157236edec7bc90f0378024da7c2d6df
Mar Pense <marcio@pense.dev>
2024-04-29 22:28:31 +0100

    add treasury

--- src/routes/tokenomics/+page.svelte
+++ src/routes/tokenomics/+page.svelte
@@ -92,7 +92,7 @@
 			<li>Max Token Supply: {Number((tokenSupply+totalMaturity+totalStakedMaturity-totalMaturityDevs)/100_000_000n).toLocaleString()} ICX</li>
 			<li>Token Supply: {Number(tokenSupply/100_000_000n).toLocaleString()} ICX</li>
 			<li>Treasury: {Number(treasury/100_000_000n).toLocaleString()} ICX</li>
-			<li>Circulating Token Supply: {Number((tokenSupply+totalMaturity+totalStakedMaturity-totalMaturityDevs-devStake-treasury)/100_000_000n).toLocaleString()} ICX</li>
+			<li>Circulating Supply: {Number((tokenSupply+totalMaturity+totalStakedMaturity-totalMaturityDevs-devStake-treasury)/100_000_000n).toLocaleString()} ICX</li>
 			<li>Neuron Stake: {Number(totalStake/100_000_000n).toLocaleString()} ICX</li>
 			<li>Maturity: {Number((totalMaturity-totalMaturityDevs)/100_000_000n).toLocaleString()} ICX</li>
 			<li>Staked Maturity: {Number(totalStakedMaturity/100_000_000n).toLocaleString()} ICX</li>
