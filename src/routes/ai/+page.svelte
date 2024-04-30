801c8cb5783ef496f4faa93dc0b026fad8b711e2
Mar Pense <marcio@pense.dev>
2024-04-30 07:03:55 +0100

    add groq

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -1,22 +1,26 @@
 <script>
-    import { Accordion, AccordionItem } from "flowbite-svelte";
+    import { Input, Button } from "flowbite-svelte";
     import Groq from 'groq-sdk';
 	import { onMount } from "svelte";
 
     const groqKey = "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"
+    const groq = new Groq({
+        apiKey: groqKey,
+        dangerouslyAllowBrowser: true
+    });
+
+    let content = ""
+    let response = ""
 
     const main = async ()  => {
-        const groq = new Groq({
-            apiKey: groqKey,
-            dangerouslyAllowBrowser: true
-        });
+        console.log("calling groq")
 
         const chatCompletion = await groq.chat.completions.create({
-            messages: [{ role: 'user', content: 'Explain the importance of low latency LLMs' }],
+            messages: [{ role: 'user', content }],
             model: 'mixtral-8x7b-32768',
         });
 
-        console.log(chatCompletion.choices[0].message.content);
+        response = chatCompletion.choices[0].message.content;
     }
 
     onMount(main)
@@ -26,3 +30,11 @@
 	<title>AI</title>
 	<meta name="description" content="AI" />
 </svelte:head>
+
+<div class="w-full">
+    <Input type="text" id="post" placeholder="Post something" bind:value={content}/>
+    <Button type="submit" on:click={main}>Submit</Button>
+</div>
+<div class="w-full">
+    {response}
+</div>
