c0a77dbe6571e8754ebf62330416ccb55c367c52
Mar Pense <marcio@pense.dev>
2024-05-02 23:04:50 +0100

    add groq

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -33,7 +33,7 @@
             }
             
             let messages = messages_tmp.slice()
-            let prompt = [{ role: 'user', content: "Generate a unique short, 50 words, response that explains why the previous messages are right, or why they are wrong, or expand with missing details, or explain better." }, { role: 'user', content }];
+            let prompt = [{ role: 'user', content: "Generate a unique short, 50 words, response that explains why the previous messages are right, or why they are wrong, or expand with missing details, or explain better the following paragraph." }, { role: 'user', content }];
             messages = messages.concat(prompt)
 
             const chatCompletion = await groq.chat.completions.create({
