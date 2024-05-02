df8c950f94b8b41c1ad31d88076488436c112d6f
Mar Pense <marcio@pense.dev>
2024-05-02 23:20:48 +0100

    add groq

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -33,7 +33,7 @@
             }
             
             let messages = messages_tmp.slice()
-            let prompt = [{ role: 'user', content: "Generate a unique, short, less than 50 words, response. Explain why the previous messages are right, and why they are wrong, and expand with missing details. If there are no previous messages, then write a response to the following paragraph." }, { role: 'user', content }];
+            let prompt = [{ role: 'user', content: "Summarize previous content in less than 300 characters. If there are no previous messages, then write a response to the following paragraph:" }, { role: 'user', content }];
             messages = messages.concat(prompt)
 
             const chatCompletion = await groq.chat.completions.create({
